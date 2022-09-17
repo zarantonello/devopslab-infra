a6c395c0e02de37741a577b13f0043fc3d141046
zarantonello <44817603+zarantonello@users.noreply.github.com>
2022-09-17 16:24:57 -0300

    Config updated

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,22 +1,11 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "firstvm" {
-  name         = "helloworld"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-8aso-zarantonello"
+  location_id = "us-central1"
+}
 
-  # Defini a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20220303a"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
-  }
+resource "google_artifact_registry_repository" "my-repo" {
+  location = "us-central1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
 }
